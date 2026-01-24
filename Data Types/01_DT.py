ba5783ef866a97c5c776f6f201c60de0492ffda2
akshay07-max <akshaysurase992@gmail.com>
2026-01-24 20:05:33 +0530

    single values data types

---
+++ Data Types/01_DT.py
@@ -0,0 +1,4 @@
+# Single valued Data type: Integer, Float, boolean, complex.
+
+# Multi valued data Types: string, list, tuple, set, dictionary.
+
---
+++ Data Types/01_single_values_DT.py
@@ -0,0 +1,18 @@
+# integer
+a = 10 # type number;
+
+print(type(a)) # <class 'int'>
+
+b = 12.3
+
+print(type(b))   # <class 'float'>
+
+c = a+7j
+
+print(type(c))   # <class 'complex'>
+
+d = True
+
+print(type(d))  # <class 'bool'>
+
+
