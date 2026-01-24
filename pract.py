fa72907d89b475706203029fd74d5f667d247499
akshay07-max <akshaysurase992@gmail.com>
2026-01-24 22:49:33 +0530

    Practice

---
+++ pract.py
@@ -0,0 +1,16 @@
+# calculate the vowels
+data = [22, "Hai", "Kya", 23, "Akshay", 32]
+
+new_list = []
+
+for i in data:
+    if type(i)==str:
+        print("hai")
+        new_list.append(i)
+    else:
+        print("nhi")
+
+
+print(new_list)
+
+
