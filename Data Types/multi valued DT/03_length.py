64ec51ba3b39939260d44f620f77c0aa256009ca
akshay07-max <akshaysurase992@gmail.com>
2026-01-25 23:53:45 +0530

    len()

---
+++ Data Types/multi valued DT/03_length.py
@@ -0,0 +1,11 @@
+# // the len(): the default function used to get the length of the string or indices present.
+# only supports multi valued data types.
+
+# syntax 
+# len(collection)\
+
+#eg:
+
+st = "The name is Khan"
+
+print(len(st))
