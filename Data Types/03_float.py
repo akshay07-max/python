9d03674eabdaadbf6a15c53a26db20ca22f0f7d6
akshay07-max <akshaysurase992@gmail.com>
2026-01-25 21:27:27 +0530

    float

---
+++ Data Types/03_float.py
@@ -0,0 +1,14 @@
+# any real number with decimal point.
+
+# eg: 1.2, 1.4, 655.3
+
+a = 10
+print(type(a))
+
+a2 = 23.11
+print(type(a2))
+
+
+# 0.0 is the default value of float
+# 1.2, -1.80 non default value for float.
+
