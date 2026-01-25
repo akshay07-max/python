2e2f84dc39d77b64bb4e1fce977666adbd477cca
akshay07-max <akshaysurase992@gmail.com>
2026-01-25 21:03:01 +0530

    basics

---
+++ feature_pyhotn.py
@@ -0,0 +1,21 @@
+# 1. Simple and easy to learn.
+# 2. Freewave and open source.
+# 3. High level programming language.
+# 4. Platform dependent
+# 5. Dynamically typed
+# 6. Interpreted,
+# 7. extensive library function (library function: functions pre-defined by developer)
+
+# Token: Tokens are the essential elements for writing the python program.
+
+
+# Keyword, Variable, Identifiers, Data Types.
+
+# Keyword: Predefined words with predefined functionality in.
+# eg: False, True, if, elif, None, except, or, for etc. 
+
+import keyword
+
+print(keyword.kwlist)
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
