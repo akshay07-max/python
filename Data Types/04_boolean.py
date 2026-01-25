0e62cf7ad4db36e0d61be3c53aaba148c3ae5d0c
akshay07-max <akshaysurase992@gmail.com>
2026-01-25 23:45:55 +0530

    strings

---
+++ Data Types/04_boolean.py
@@ -0,0 +1,16 @@
+# truthy and falsy values.
+# True and False;
+
+# a = True
+# print(type(a))
+# print(a)
+
+
+# validation 
+a = 10
+b = 0
+
+print(bool(a))  #False
+print(bool(b))  #False
+
+bool(0.0)   #False
---
+++ Data Types/multi valued DT/01_string.py
@@ -0,0 +1,12 @@
+# String is a collection of characters.
+# Declared using " " (double quat) or ' ' (single quat) or """ """ (triple quat)
+
+# String is IMMUTABLE data type.
+
+# varname = 'I am groot'
+# varname = "I am groot"
+# varname = """I am groot"""
+
+name = "Akshay"
+print(type(name))
+
