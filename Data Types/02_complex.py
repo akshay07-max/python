992741113615159fa3b8b5537028a4c30dd5cad6
akshay07-max <akshaysurase992@gmail.com>
2026-01-25 23:34:07 +0530

    complex

---
+++ Data Types/02_complex.py
@@ -0,0 +1,12 @@
+# Combination of real numbers and imaginary numbers.
+
+# A + Bj
+# A => refers real part
+# Bj => imaginary;
+# where A and B can be the int or float.
+
+a = 1.2 + 2j
+print(type(a))
+print(a)
+
+# 0j default for complex numbers.
