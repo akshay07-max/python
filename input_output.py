950838d373ecefcc18566196cbd1fe8596d3c710
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-26 22:49:20 +0530

    Update input_output.py

--- input_output.py
+++ input_output.py
@@ -6,3 +6,11 @@ print(a)
 # print multiple values
 print("Name": "akshay", age: 24, "Gmader": "Male")
 
+
+# with vars
+name = "Akshay"
+age = 24
+DOB = "7th Jan"
+gender = "male"
+
+print("Name": name, "Age": age, "DOB": DOB, "Gender": gender)
