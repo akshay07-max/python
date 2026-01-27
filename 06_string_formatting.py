d084d261d634229070964452fa8f0d09f8fcf580
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-27 19:56:44 +0530

    Create 06_string_formatting.py
    f string

---
+++ 06_string_formatting.py
@@ -0,0 +1,10 @@
+# there are three ways to format a string
+#1. f- string
+
+name = "Akshay"
+age = 24
+gender = "Male"
+
+message = f"Hello, My name is {name}, i am {age} years old {gender})
+
+print(message)
