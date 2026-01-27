9c7384b9f49f990cdc5b606dcb8f31b137186378
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-27 20:07:46 +0530

    Update 06_string_formatting.py
    .format

--- 06_string_formatting.py
+++ 06_string_formatting.py
@@ -25,3 +25,21 @@ print(f"Price: ${price:.2f}")
 population = 1000000
 print(f"Population: {population:,}")
 # Output: Population: 1,000,000
+
+# 2. format (old but working)
+name1 = "Akshay"
+age1 = 24
+
+message= "My name is {}, and I'm {} years old".format(name, age)
+
+print(message)
+
+# With index numbers
+message = "My name is {0}, I am {1} years old. Yes, {0}!".format(name, age)
+print(message)
+# Output: My name is Bob, I am 30 years old. Yes, Bob!
+
+# With names
+message = "My name is {n}, I am {a} years old".format(n=name, a=age)
+print(message)
+# Output: My name is Bob, I am 30 years old
