b593e8cd0e673e04668d43e116f6c4075c89737f
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-27 19:45:23 +0530

    Update 05_escape_sequence.py

--- 05_escape_sequence.py
+++ 05_escape_sequence.py
@@ -5,3 +5,11 @@ print("apple", "Banana", "Watermelon", sep="\n")
 # tab or horizontal spacing
 print("Name:\tAkshay")
 print("Age:\t24")
+
+
+print("Common Escape Sequences:")
+print("\\n - Newline")
+print("\\t - Tab")
+print("\\\\ - Backslash")
+print("\\\" - Double quote")
+print("\\' - Single quote")
