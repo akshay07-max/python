d7b1b180861fd3f55d935f558b054bc72ecb8795
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-27 19:40:32 +0530

    Create 04_end.py
    end with print

---
+++ 04_end.py
@@ -0,0 +1,15 @@
+# by default the print ends with the new line
+print("Hello")
+print("world") #world will be printted in the next line
+
+# custome end char
+print("hello", end=" ")
+print("World")
+
+print("Loading", end="")
+print("...")
+
+print("a", end="=")
+print("b")
+
+
