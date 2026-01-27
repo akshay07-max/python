eee7bdeb89550e577d91687a4c1d57719e58f9f0
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-27 20:00:36 +0530

    Update 06_string_formatting.py

--- 06_string_formatting.py
+++ 06_string_formatting.py
@@ -8,3 +8,20 @@ gender = "Male"
 message = f"Hello, My name is {name}, i am {age} years old {gender})
 
 print(message)
+
+a= 100
+b = 20
+print(f"sum: {a+b})
+
+# formatting numbers
+pi =3.1415
+print(f"PI rounder: {pi: .2f})
+
+price = 1234.5
+print(f"Price: ${price:.2f}")
+# Output: Price: $1234.50
+
+# Thousands separator
+population = 1000000
+print(f"Population: {population:,}")
+# Output: Population: 1,000,000
