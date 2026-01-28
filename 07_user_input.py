a8553e64a55b595f782b31a5e297a806ee62cd3a
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-28 20:30:01 +0530

    Update 07_user_input.py

--- 07_user_input.py
+++ 07_user_input.py
@@ -9,3 +9,14 @@ print("Hello,", name)
 When you run this:
 What is your name? Alice
 Hello, Alice
+
+
+Important: input() Always Returns a String!
+This is VERY important to understand:
+# Taking age as input
+age = input("Enter your age: ")
+print(type(age))  # <class 'str'> ← It's a string!
+
+# This will NOT work as expected:
+next_age = age + 1  # ERROR! Can't add 1 to string
+Why? Because input() always returns a string, even if you type a number!
