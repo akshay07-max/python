f705e3d6f5c43b78f8c4c0d892bca437b4ce938c
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-28 20:31:17 +0530

    Create 08_type_casting.py
    type casting, converting data types

---
+++ 08_type_casting.py
@@ -0,0 +1,7 @@
+Type Casting (Converting Data Types)
+To use input as numbers, we must convert (cast) them!
+Type Casting Functions:
+int() - Convert to integer
+float() - Convert to float
+str() - Convert to string
+bool() - Convert to boolean
