76ce11179ceae962a9c3fc25f3321c075a0fd6b0
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-01-29 18:25:55 +0530

    Create 12_comments.py

---
+++ 12_comments.py
@@ -0,0 +1,12 @@
+Why Comments are Important
+Without comments:
+x = 5280
+y = x * 1.60934
+print(y)
+What does this do? No idea!
+With comments:
+# Convert miles to kilometers
+miles = 5280
+kilometers = miles * 1.60934  # 1 mile = 1.60934 km
+print(kilometers)
+Ah! Now it's clear!
