bf3056ed0d1d09162cda59eb8032dd817ee05670
akshay07-max <akshaysurase992@gmail.com>
2026-01-30 23:10:11 +0530

    self keyword

---
+++ Classes/06_self.py
@@ -0,0 +1,16 @@
+# The self parameter is a reference to the current instance of the class.
+
+# It is used to access properties and methods that belong to the class.
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Emil", 25)
+p1.greet()
+
+# Note: The self parameter must be the first parameter of any method in the class.
