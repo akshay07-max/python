9df90557fbd3056aea7f204b44d5aa0a3103dd89
akshay07-max <akshaysurase992@gmail.com>
2026-01-30 22:44:52 +0530

    del class

--- Classes/01_class.py
+++ Classes/01_class.py
@@ -6,7 +6,18 @@ class Bank:
     atm=True
 
 
+    
+
+
 fBank = Bank()
 fBank.location = "Chennai"
 
+gBank = Bank()
+
+
+del fBank  # deletes the fBank instance of class Bank
+
+
+
 print(f"Bank is located in {fBank.location}")
+print(f"Bank is located in {gBank.location}")
