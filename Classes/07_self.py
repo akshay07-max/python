6ed07772e2644d938a5f4eaaef96961c11955e10
akshay07-max <akshaysurase992@gmail.com>
2026-02-01 20:43:34 +0530

    self

--- Classes/07_self.py
+++ Classes/07_self.py
@@ -4,8 +4,11 @@ class BioData:
         self.age = age
 
     def greet(self):
-        print(f"Hello, Good Morning. My name is {self.name}, and I'm {self.age} years old");
+        return(f"Hello, Good Morning. My name is {self.name}, and I'm {self.age} years old");
+
+    def grt(self):
+        return(f"Hello good morning..")
 
 p1 = BioData("Akshay", 23)
-
 print(p1.greet())
+print(p1.grt())
