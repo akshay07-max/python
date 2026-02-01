bb4184b4da75b22d9074c4c8528597426f49aecf
akshay07-max <akshaysurase992@gmail.com>
2026-02-01 21:27:00 +0530

    accessing property with dot

--- Classes/Class Properties/01_props.py
+++ Classes/Class Properties/01_props.py
@@ -1,1 +1,12 @@
 # Properties  the variables that belongs to the class. they store data  for each object created from the class.
+
+# accessing object property with dot
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+car1 = Car("Toyota", "Corolla")
+
+print(car1.brand)
+print(car1.model)
