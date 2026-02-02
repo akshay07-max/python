9e6bbe21035df50ed17cee977114d6ba9c28f6e4
akshay07-max <akshaysurase992@gmail.com>
2026-02-02 22:16:09 +0530

    __str__

--- Classes/Class Methods/05_str_.py
+++ Classes/Class Methods/05_str_.py
@@ -9,3 +9,16 @@ class Person:
 p1 = Person("Emil", 36)
 print(p1)
 
+
+# With the __str__() method:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name} ({self.age})"
+
+p1 = Person("Tobias", 36)
+print(p1)
