21dffcbea2d702dcf643990aa9a84763a6c3c055
akshay07-max <akshaysurase992@gmail.com>
2026-02-03 08:57:28 +0530

    inheritance intro

---
+++ Inheritance/01_intro.py
@@ -0,0 +1,5 @@
+# inheritance allows us to define a class which inherits all the methods and properties form another class.
+
+# Parent class is the class being inherited from, also called base class.
+
+# Child class is the class that inherits from another class, also called derived class.
