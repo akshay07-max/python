fbfc985db59edaa14b8897142e8726d76ca19de8
akshay07-max <akshaysurase992@gmail.com>
2026-02-04 17:54:39 +0530

    Polymorphism

---
+++ Polymorphism/01_intro.py
@@ -0,0 +1,4 @@
+# The word "polymorphism" means "many forms", and in programming it refers to methods/functions/operators with the same name that can be executed on many objects or classes.
+
+# the length (len()) function is the good example of the polymorphism.
+
