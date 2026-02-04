8d77fec816d4080f5e5ad4228ca4ef161186e248
akshay07-max <akshaysurase992@gmail.com>
2026-02-04 16:04:44 +0530

    child inheritance

--- Inheritance/04_child_init.py
+++ Inheritance/04_child_init.py
@@ -4,7 +4,7 @@
 
 class Person:
     def __init__(self, fname, lname):
-        self.name = fname
+        self.fname = fname
         self.lname = lname
 
     def fullName(self):
@@ -14,14 +14,32 @@ class Person:
 # When you add the __init__() function, the child class will no longer inherit the parent's __init__() function
 # Note: The child's __init__() function overrides the inheritance of the parent's __init__() function.
 
+# class Student(Person):
+#     def __init__(self, fname, lname):
+#         self.fname = fname
+#         self.lname = lname
+
+#     def stdName(self):
+#         return f"{self.fname} {self.lname}"
+    
+# std1 = Student("Akshay", "Surase")
+
+# print(std1.stdName())
+
+# To keep the inheritance of the parent's __init__() function, add a call to the parent's __init__() function:
+
 class Student(Person):
     def __init__(self, fname, lname):
+        Person.__init__(self, fname, lname)
         self.fname = fname
         self.lname = lname
 
-    def stdName(self):
-        return f"{self.fname} {self.lname}"
     
-std1 = Student("Akshay", "Surase")
 
-print(std1.stdName())
+S1 = Student("Akshay", "Surase")
+
+print(S1.fullName())
+
+
+# As you can see in the code we need to redeclare the person's properties duw to the init of the child class
+# let's know about the super in python to overcome this.
