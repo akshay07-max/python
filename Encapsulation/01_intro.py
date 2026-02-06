908ca3eb2fe551298c904197d50ddff85cbfb1ba
akshay07-max <akshaysurase992@gmail.com>
2026-02-06 09:38:46 +0530

    Encapsulation in python

---
+++ Encapsulation/01_intro.py
@@ -0,0 +1,4 @@
+# The encapsulation in python is keeping the main class data safe while accessing from the outside
+# It is about the protecting the data inside a class.
+# It means keeping data (properties) and methods together in a class, while controlling how the data can be accessed from outside the class.
+# This prevents accidental changes to your data and hides the internal details of how your class works.
