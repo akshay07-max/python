f85b8ef8fe5973f5e3bc308aa2784d39ad4f2ae0
akshay07-max <akshaysurase992@gmail.com>
2026-02-06 09:49:28 +0530

    encapsulation, private prop

---
+++ Encapsulation/03_access_private_prop.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age
+
+    def get_age(self):
+        return self.__age
+    
+
+p1 = Person("Akshay", 23)
+
+print(p1.get_age())
+
