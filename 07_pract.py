dabb67a900e35d9dd8dbac8b0aecd990b83e9586
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-02-08 23:02:04 +0530

    Create 07_pract.py
    i heritance eg

---
+++ 07_pract.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    def start(self):
+        print("Vehicle started")
+
+
+class Car(Vehicle):
+    def drive(self):
+        print("Car is driving")
+
+
+c = Car()
+c.start()   # inherited
+c.drive()
