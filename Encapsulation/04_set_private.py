941001f1c306e7c562c244c3964cb24ceb31f703
akshay07-max <akshaysurase992@gmail.com>
2026-02-08 09:24:30 +0530

    setting private propery

--- Encapsulation/04_set_private.py
+++ Encapsulation/04_set_private.py
@@ -6,5 +6,18 @@ class Person:
     def get_age(self):
         return self.__age
     
-    def set_age(self):
-        
+    def set_age(self, age):
+        if(age>0):
+            self.__age = age
+        else:
+            return "Invalid Age"
+
+
+p1 = Person("Akshay", 23)
+
+totalAge = p1.get_age()
+print(totalAge)
+
+p1.set_age(34)
+
+print(p1.get_age())
