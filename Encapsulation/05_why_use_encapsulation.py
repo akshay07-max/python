5b4aa484fe6c3c69b26a67ad6cb0536772980c69
akshay07-max <akshaysurase992@gmail.com>
2026-02-08 10:44:33 +0530

    why use encapsulation

---
+++ Encapsulation/05_why_use_encapsulation.py
@@ -0,0 +1,6 @@
+# Encapsulation provides:
+# Data protection: prevents accidental data modification,
+# validation: Can validate data before setting it.
+# Flexibility: Internal implementation can change without external changes.
+# control: you have full access over how the data is accessed and modified.
+
