8001d2fa28fc909dd1b286758084646e10f854c4
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-02-08 23:07:53 +0530

    Create 13_pract.py

---
+++ 13_pract.py
@@ -0,0 +1,15 @@
+Summary (Easy to Remember)
+Concept
+Meaning
+Class
+Blueprint
+Object
+Instance
+Encapsulation
+Data protection
+Inheritance
+Code reuse
+Polymorphism
+Same method, different behavior
+Abstraction
+Hide implementation
