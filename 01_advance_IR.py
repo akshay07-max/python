1a8c51e75666f8d3eb5897ff165f3e30d6eee8cf
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-02-10 20:18:16 +0530

    Create 01_advance_IR.py

---
+++ 01_advance_IR.py
@@ -0,0 +1,12 @@
+# in the file name IR stands for Interview Ready.
+
+class User:
+    def __init__(self, name):
+         self.name=name
+
+u = User("Akshay")
+print(u)
+
+# this js ugly coding.
+
+# if your class returning a string then used __str__().
