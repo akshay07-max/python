f88c44be74da0a5b02ed39cfaf8ddd20db187613
akshay07-max <akshaysurase992@gmail.com>
2026-02-14 20:47:50 +0530

    set private property

--- Encapsulation/06_validation.py
+++ Encapsulation/06_validation.py
@@ -3,4 +3,20 @@ class Student:
         self.name = name
         self.__grade = grade
 
-    def set_grade()
+    def get_grade(self):
+        return self.__grade
+
+    def set_grade(self, grade):
+        self.__grade = grade
+
+
+s1 = Student("Akshay", 89)
+print(s1.name)
+# print(s1.__grade) # Error
+print(s1.get_grade())  # grade = 89
+
+s1.set_grade(90)
+
+print(s1.get_grade()) # grade = 90
+
+
