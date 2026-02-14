ea8150cacbbd16f39a4cbc8b40fa6b7124a50f2f
akshay07-max <akshaysurase992@gmail.com>
2026-02-14 21:14:43 +0530

    name mangling

--- Encapsulation/08_Name_Mangling.py
+++ Encapsulation/08_Name_Mangling.py
@@ -3,3 +3,14 @@
 # When you use double underscores __, Python automatically renames it internally by adding _ClassName in front.
 
 # For example, __age becomes _Person__age.
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+p1 = Person("Emil", 30)
+
+# This is how Python mangles the name:
+print(p1._Person__age) # Not recommended!
