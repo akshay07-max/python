1c376d545d66a6b8aef9a2c43346a6302027b99c
akshay07-max <akshaysurase992@gmail.com>
2026-02-14 21:14:12 +0530

    Encapsulation

---
+++ Encapsulation/08_Name_Mangling.py
@@ -0,0 +1,5 @@
+# Name mangling is how Python implements private properties and methods.
+
+# When you use double underscores __, Python automatically renames it internally by adding _ClassName in front.
+
+# For example, __age becomes _Person__age.
