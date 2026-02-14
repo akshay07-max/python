dee7d216cca4f7c4d0ec7f43b8b6a0c954b3d91f
akshay07-max <akshaysurase992@gmail.com>
2026-02-14 21:17:03 +0530

    encapsulation pract

--- Encapsulation/08_Name_Mangling.py
+++ Encapsulation/08_Name_Mangling.py
@@ -8,7 +8,7 @@
 class Person:
   def __init__(self, name, age):
     self.name = name
-    self.__age = age
+    self._age = age
 
 p1 = Person("Emil", 30)
 
---
+++ Encapsulation/09_Challenge_Encapsulation.py
@@ -0,0 +1,6 @@
+# Inside the editor, complete the following steps:
+# Create a class BankAccount
+# Add an __init__ that takes balance and stores it as self.__balance (private)
+# Add a method get_balance that returns self.__balance
+# Create an object acc = BankAccount(1000)
+# Print the result of acc.get_balance()
