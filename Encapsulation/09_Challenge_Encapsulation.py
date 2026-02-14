c97d122f59e45fdf999c09c4343664eddec32f02
akshay07-max <akshaysurase992@gmail.com>
2026-02-14 21:20:02 +0530

    encapsulation pract

--- Encapsulation/09_Challenge_Encapsulation.py
+++ Encapsulation/09_Challenge_Encapsulation.py
@@ -8,4 +8,11 @@
 class BankAccount:
     def __init__(self, balance):
         self.__balance = balance
-        
+
+    def get_balance(self):
+        return self.__balance
+    
+
+acc = BankAccount(2000);
+
+print(acc.get_balance())
