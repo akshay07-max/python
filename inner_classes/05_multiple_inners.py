4dec610fbd4e8b16fc33b630486ef199720ef81a
akshay07-max <akshaysurase992@gmail.com>
2026-02-15 23:01:26 +0530

    accessing outer calss from inner

---
+++ inner_classes/05_multiple_inners.py
@@ -0,0 +1,22 @@
+# Multiple Inner Classes
+# A class can have multiple inner classes:
+
+# Example
+# Create multiple inner classes:
+
+class Computer:
+  def __init__(self):
+    self.cpu = self.CPU()
+    self.ram = self.RAM()
+
+  class CPU:
+    def process(self):
+      print("Processing data...")
+
+  class RAM:
+    def store(self):
+      print("Storing data...")
+
+computer = Computer()
+computer.cpu.process()
+computer.ram.store()
