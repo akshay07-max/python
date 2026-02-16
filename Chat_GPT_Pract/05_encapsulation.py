25f03ac423d2e948ffb76d21c82051402a88da1c
akshay07-max <akshaysurase992@gmail.com>
2026-02-16 18:01:58 +0530

    chat gpt practice

--- Chat_GPT_Pract/05_encapsulation.py
+++ Chat_GPT_Pract/05_encapsulation.py
@@ -8,3 +8,17 @@
 
 # Private
 # self.__password
+
+
+
+class Bank:
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def get_balance(self):
+        return self.__balance
+
+
+# Accessing private directly:
+b = Bank(1000)
+print(b._Bank__balance)  # Name mangling
