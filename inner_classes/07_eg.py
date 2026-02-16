878ab3e6809413de0721067b19059728dc0db8c8
akshay07-max <akshaysurase992@gmail.com>
2026-02-16 17:48:03 +0530

    accessing outer class

---
+++ inner_classes/07_eg.py
@@ -0,0 +1,15 @@
+class Outer:
+    def __init__(self):
+      self.name = "Akshay"
+
+    class Inner:
+        def __init__(self, outer):
+            self.outer = outer
+
+        def display(self):
+            print(f"Outer class name: {self.outer.name}")
+
+
+outer = Outer()
+inner = outer.Inner(outer)
+inner.display()
