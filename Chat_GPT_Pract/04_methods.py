fb1a1be16603e20b4f774fba4429d935ed1ec943
akshay07-max <akshaysurase992@gmail.com>
2026-02-16 18:00:47 +0530

    chat gpt practice

--- Chat_GPT_Pract/04_methods.py
+++ Chat_GPT_Pract/04_methods.py
@@ -1,3 +1,20 @@
 class Demo:
     def show(self):
         print("Instance method")
+
+
+class Demo:
+    count = 0
+
+    @classmethod
+    def increment(cls):
+        cls.count += 1
+
+
+# Static Method:
+
+# No access to class or instance
+class Math:
+    @staticmethod
+    def add(a, b):
+        return a + b
