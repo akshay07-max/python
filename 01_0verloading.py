4026e6e739b9726cac066175388adac1e5758023
akshay07-max <akshaysurase992@gmail.com>
2026-02-17 17:17:35 +0530

    method overriding and overloading

---
+++ 01_0verloading.py
@@ -0,0 +1,2 @@
+# In Python, method overloading involves creating methods with the same name but different parameters within the same class (mimicked through workarounds), while method overriding involves a subclass redefining a method from its parent class
+
