20c1139df2274875e1e98d548361b96eecb9ecc8
akshay07-max <akshaysurase992@gmail.com>
2026-02-17 17:21:00 +0530

    method overriding and overloading

--- 02_overriding.py
+++ 02_overriding.py
@@ -7,4 +7,8 @@ class A:
 
 
 class B(A):
-    
+    def fun1(self):
+        print("Modifying feature_1 of class A by class B")
+
+    def fun2(self):
+        print("Modifying feature_2 of class A by class B")
