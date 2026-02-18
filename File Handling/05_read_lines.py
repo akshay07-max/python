c2fbc8732d420955611d55f89156b20407da0f1e
akshay07-max <akshaysurase992@gmail.com>
2026-02-18 09:49:01 +0530

    file handling: read parts with readline and with

---
+++ File Handling/05_read_lines.py
@@ -0,0 +1,5 @@
+with open("File Handling/file.txt") as f:
+    print(f.readline())
+    print(f.readline()) # reads next line not the same one (2rd line)
+    print(f.readline()) # reads next line not the same one (3rd line)
+
