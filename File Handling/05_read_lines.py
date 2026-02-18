acdc373fc6cc5ae64c259c3dc709232068d89499
akshay07-max <akshaysurase992@gmail.com>
2026-02-18 09:50:11 +0530

    file handling: read parts using loop

--- File Handling/05_read_lines.py
+++ File Handling/05_read_lines.py
@@ -3,3 +3,11 @@ with open("File Handling/file.txt") as f:
     print(f.readline()) # reads next line not the same one (2rd line)
     print(f.readline()) # reads next line not the same one (3rd line)
 
+
+# using loops
+
+with open("File Handling/file.txt") as f:
+    for x in f:
+        print(x)
+
+
