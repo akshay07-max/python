a21c980f979298a2b5730b620ae577d2ae5e93f5
akshay07-max <akshaysurase992@gmail.com>
2026-02-18 10:00:00 +0530

    file handling: using a(append)

--- File Handling/06_append_write_create.py
+++ File Handling/06_append_write_create.py
@@ -8,7 +8,7 @@
 
 
 with open("File Handling/demotxt.txt", "a") as f:
-    f.write("This is the text I'm writing in demotxt file usinf a(append), it will append to the existing content.")
+    f.write("This is the text I'm writing in demotxt file usinf \'a\'(append), it will append to the existing content.")
 
 with open("File Handling/demotxt.txt") as f:
     print(f.read())
