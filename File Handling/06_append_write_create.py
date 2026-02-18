eb11d30dc066ba24c525f4e813bda7c84e5af0a9
akshay07-max <akshaysurase992@gmail.com>
2026-02-18 09:54:56 +0530

    file handling: write/create/append

--- File Handling/06_append_write_create.py
+++ File Handling/06_append_write_create.py
@@ -2,6 +2,9 @@
 
 # To write to an existing file, you must add a parameter to the open() function:
 
-# "a" - Append - will append to the end of the file
+# "a" - Append - will append to the end of the file.
 
-# "w" - Write - will overwrite any existing content
+# "w" - Write - will overwrite any existing content.
+
+
+with open("demotxt.txt")
