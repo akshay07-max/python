fea3dc2a211fe8f1c831145d083e0e5ef27e14ec
akshay07-max <akshaysurase992@gmail.com>
2026-02-22 09:47:22 +0530

    numpy arrays

---
+++ Numpy/03_numpy_array.py
@@ -0,0 +1,15 @@
+# Create a NumPy ndarray Object
+# NumPy is used to work with arrays. The array object in NumPy is called ndarray.
+
+# We can create a NumPy ndarray object by using the array() function.
+
+import numpy as np
+
+arr = np.array([11, 22, 33, 44, 55])
+
+print(arr)          # [11 22 33 44 55]
+print(type(arr))    # <class 'numpy.ndarray'>
+
+
+
+# type(): This built-in Python function tells us the type of the object passed to it. Like in above code it shows that arr is numpy.ndarray type.
