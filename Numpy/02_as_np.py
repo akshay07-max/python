a844cd2b3206a5a67fb2b2de05e4b57104eaa918
akshay07-max <akshaysurase992@gmail.com>
2026-02-22 09:42:11 +0530

    numpy as np

---
+++ Numpy/02_as_np.py
@@ -0,0 +1,11 @@
+# NumPy as np
+# NumPy is usually imported under the np alias.
+
+# alias: In Python alias are an alternate name for referring to the same thing.
+
+# Create an alias with the as keyword while importing:
+
+import numpy as np
+
+arr = np.array([22,22,33,44,55])
+print(arr)
