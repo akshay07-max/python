6027a27d57f55bbcf25304369fb122ff5a10d2e1
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-02-23 20:04:00 +0530

    Create 01_pract_numpy_022.py

---
+++ 01_pract_numpy_022.py
@@ -0,0 +1,11 @@
+# 01_pract_numpy_022.py
+# dimensions 
+
+import numpy as np
+
+arr0 = np.array(10) # zero dimensional
+print(arr0)
+
+arr1 = np.array([12, 13, 14, 15, 16]) # 1-D array
+print(arr1)
+
