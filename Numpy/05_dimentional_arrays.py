e29e891cd0fea7b9d7d766bb0cde00562ea57391
akshay07-max <akshaysurase992@gmail.com>
2026-02-23 09:42:02 +0530

    0-3 dimensional arrays

--- Numpy/05_dimentional_arrays.py
+++ Numpy/05_dimentional_arrays.py
@@ -18,3 +18,14 @@ print(onArr)
 # 2-D array. An array which has 1-D as its element is called 2-D array
 twArr = np.array([[12,23, 23], [33, 44, 55]])
 print(twArr)
+
+# These are often used to represent matrix or 2nd order tensors.
+# NumPy has a whole sub module dedicated towards matrix operations called numpy.mat
+
+# 3-D array 
+# An array that has 2-D arrays (matrices) as its elements is called 3-D array.
+# These are often used to represent a 3rd order tensor.
+
+trArr = np.array([[[3, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+print(trArr)
+
