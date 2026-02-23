89f4fbe80b6a4fa86aef97d93e4b95cada225f64
akshay07-max <akshaysurase992@gmail.com>
2026-02-23 09:35:09 +0530

    2-D array

--- Numpy/05_dimentional_arrays.py
+++ Numpy/05_dimentional_arrays.py
@@ -14,3 +14,7 @@ print(zrArr)
 # 1-D array
 onArr = np.array([1, 23,444])
 print(onArr)
+
+# 2-D array. An array which has 1-D as its element is called 2-D array
+twArr = np.array([[12,23, 23], [33, 44, 55]])
+print(twArr)
