29127111cbb1c8e2c595ff34e23f2e1b6449ad08
akshay07-max <akshaysurase992@gmail.com>
2026-02-23 09:33:12 +0530

    0-1 dimensional arrays

--- Numpy/05_dimentional_arrays.py
+++ Numpy/05_dimentional_arrays.py
@@ -2,3 +2,15 @@
 # A dimension in arrays is one level of array depth (nested arrays).
 
 # nested array: are arrays that have arrays as their elements.
+
+import numpy as np
+
+# 0 (zero) dimensional
+zrArr = np.array(9)
+
+print(zrArr)
+
+
+# 1-D array
+onArr = np.array([1, 23,444])
+print(onArr)
