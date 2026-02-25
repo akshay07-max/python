93416faf554fbb66e2ef8d9467cc7594563372d1
akshay07-max <akshaysurase992@gmail.com>
2026-02-25 16:01:19 +0530

    slicing in numpy

--- Numpy/Array Slicing/01_slicing.py
+++ Numpy/Array Slicing/01_slicing.py
@@ -3,3 +3,11 @@ import numpy as np
 arr = np.array([12, 13, 14, 15, 16])
 
 print(arr[1:4])
+
+# Note: The result includes the start index, but excludes the end index.
+
+# Slice elements from index 4 to the end of the array:
+print(arr[4:])
+
+# Slice elements from the beginning to index 4 (not included):
+print(arr[:4])
