dbcb5daf5a5c410fde2e898a1aabc69b4bea98b6
akshay07-max <akshaysurase992@gmail.com>
2026-02-25 15:56:55 +0530

    slicing in numpy

---
+++ Numpy/Array Slicing/01_intro.py
@@ -0,0 +1,12 @@
+# Slicing arrays
+# Slicing in python means taking elements from one given index to another given index.
+
+# We pass slice instead of index like this: [start:end].
+
+# We can also define the step, like this: [start:end:step].
+
+# If we don't pass start its considered 0
+
+# If we don't pass end its considered length of array in that dimension
+
+# If we don't pass step its considered 1
