36c55bf34116720eea68653e173d0eff5b2e2f75
akshay07-max <akshaysurase992@gmail.com>
2026-02-25 15:50:34 +0530

    3-D array

--- Numpy/Array_Indexing/03_3-D_array.py
+++ Numpy/Array_Indexing/03_3-D_array.py
@@ -1,3 +1,4 @@
+# To access elements from 3-D arrays we can use comma separated integers representing the dimensions and the index of the element.
 import numpy as np
 
 three_d_array = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
