1f665c854bda9abce5accf1700c95ee1f64abe23
akshay07-max <akshaysurase992@gmail.com>
2026-02-27 20:35:57 +0530

    array dim

---
+++ Numpy/NUMPY_RE.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+arr = np.array([23, 13, 11, 22, 26])
+
+# print(arr)
+
+# element accessing
+# print(arr[0])
+# print(arr[1])
+# print(arr[2])
+# print(arr[3])
+# print(arr[-1])
+
+# Dimentional arrays
+
+# 0-D
+zr_d = np.array(89)
+# print(zr_d)
+# print(np.ndim(zr_d))
+
+# 1-D 
+on_d = np.array([12,13, 14])
+# print(on_d)
+print(np.ndim(on_d))
+
+# 2-D 
