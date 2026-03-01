12a20b2930c8132bd8af850d10b1f14b7ea20812
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-03-01 21:16:03 +0530

    Update 10_pract_numpy.py

--- 10_pract_numpy.py
+++ 10_pract_numpy.py
@@ -1,8 +1,16 @@
 Level 4: Statistical Operations
 1️⃣1️⃣ Find mean, median, std deviation
 
+# find mean, median, mode
 arr = np.array([1, 2, 3, 4, 5])
 
 print("Mean:", np.mean(arr))
 print("Median:", np.median(arr))
 print("Std Dev:", np.std(arr))
+
+# find max and its index
+
+arr = np.array([10, 25, 5, 40, 30])
+
+print("Max:", np.max(arr))
+print("Index:", np.argmax(arr))
