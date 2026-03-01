d97a84b855b8bbf49f028d76098a6c67888972b9
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-03-01 21:15:18 +0530

    Update 10_pract_numpy.py

--- 10_pract_numpy.py
+++ 10_pract_numpy.py
@@ -1,2 +1,8 @@
 Level 4: Statistical Operations
 1️⃣1️⃣ Find mean, median, std deviation
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print("Mean:", np.mean(arr))
+print("Median:", np.median(arr))
+print("Std Dev:", np.std(arr))
