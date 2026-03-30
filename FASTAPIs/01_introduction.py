f680bf139636a22337d7aafa434e25ad88df0dc4
akshay07-max <akshaysurase992@gmail.com>
2026-03-30 15:41:47 +0530

    introcduction to fast api

---
+++ FASTAPIs/01_introduction.py
@@ -0,0 +1,17 @@
+# - One of the fastest python framework, leveraging async programming to handle multiple request at a time.
+# Starlette and Pydantic Based: Uses Starlette for web features (routing, middleware, WebSockets) and Pydantic for type-based validation and parsing.   
+# Auto API Docs: Generates interactive documentation (Swagger UI, ReDoc) automatically for easy testing.
+# Async-First Design: Built around async/await for high concurrency, suitable for real-time and scalable applications.
+
+
+# Installation for FastAPIs
+
+# step 1:
+# pip instal fastapi
+
+# step 2:
+# FastAPI needs and ASGI server to run. The most common recommended one is UVICORN.
+# pip install "uvicorn[standard]"
+
+# step 3:
+# First FastAPI App:
