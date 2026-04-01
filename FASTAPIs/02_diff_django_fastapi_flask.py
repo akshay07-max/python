ccee52a783a96e375ba51fc1fe366e7284ec4088
akshay07-max <akshaysurase992@gmail.com>
2026-04-01 10:11:05 +0530

    diff betw django, fastapi, flask

---
+++ FASTAPIs/02_diff_django_fastapi_flask.py
@@ -0,0 +1,27 @@
+# Django
+# Django is a full-stack web framework written in Python. It follows MTV (Model-Template-View) architectural pattern and promotes DRY principle (Don’t Repeat Yourself).
+
+
+# Released in 2005 by Adrian Holovaty and Simon Willison.
+# Named after the jazz guitarist Django Reinhardt.
+# Comes with everything built-in: authentication, ORM (Object Relational Mapper), admin panel and security features.
+# When to use it?
+# If you are building a large, feature-rich web application like an e-commerce platform, social media app or content management system, Django is often the best choice.
+
+# Flask
+# Flask is a micro web framework written in Python. Unlike Django, it doesn’t come with many built-in tools; instead, it gives developers flexibility to choose their own libraries.
+
+# Released in 2010 by Armin Ronacher (Pocoo team).
+# Based on the Werkzeug WSGI toolkit and Jinja2 template engine.
+# Known for being lightweight and beginner-friendly.
+# When to use it?
+# If you’re creating a small to medium web application or want more freedom and flexibility in choosing components (like ORM, authentication, etc.), Flask is a great option.
+
+# FastAPI
+# FastAPI is a modern, high-performance web framework for building APIs with Python. It’s built on Starlette (for web handling) and Pydantic (for data validation).
+
+# Released in 2018 by Sebastián Ramírez.
+# Uses Python type hints to automatically validate input data.
+# Fully supports asynchronous programming, making it extremely fast.
+# When to use it?
+# If you are working on REST APIs, microservices or machine learning model deployment, FastAPI is an excellent choice due to its speed and efficiency.
