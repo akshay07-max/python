fad142fa519ba539b7f7155bae7fa6cba4b6aa11
akshay07-max <akshaysurase992@gmail.com>
2026-04-08 11:38:39 +0530

    non convertable values

--- Numpy/Data Types/04_non_convertable.py
+++ Numpy/Data Types/04_non_convertable.py
@@ -1,6 +1,11 @@
 # What if a Value Can Not Be Converted?
 import numpy as np
 
-arr = np.array(['a', '3', '4'], dtype="i")
+# arr = np.array(['a', '3', '4'], dtype="i")  # gives value error.
 
-print(arr.dtype)
+# print(arr.dtype)
+
+
+arr_any = np.array([12, "Apple"]) # 12 is converted to string
+print(arr_any)
+print(arr_any.dtype) # unicode string
