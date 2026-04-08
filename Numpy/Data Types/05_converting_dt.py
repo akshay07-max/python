94a34816ead940f477847aa3498c76899dc9813e
akshay07-max <akshaysurase992@gmail.com>
2026-04-08 11:44:47 +0530

    changing datatypes of existing array astype()

--- Numpy/Data Types/05_converting_dt.py
+++ Numpy/Data Types/05_converting_dt.py
@@ -8,11 +8,33 @@ import numpy as np
 
 # The data type can be specified using a string, like 'f' for float, 'i' for integer etc. or you can use the data type directly like float for float and int for integer.
 
+
+# Change data type from float to integer by using 'i' as parameter value:
 arr = np.array([1.1, 2.1, 3.1])
 
 newarr = arr.astype('i') # int32
 # newarr = arr.astype('i4') # int32
 # newarr = arr.astype('i8') # int64
+
+print(newarr)
+print(newarr.dtype)
+
+
+# Change data type from float to integer by using int as parameter value:
+arr = np.array([1.1, 2.1, 3.1])
+
+newarr = arr.astype(int)
+
+print(newarr)
+print(newarr.dtype)
+
+
+
+
+# Change data type from integer to boolean:
+arr = np.array([1, 0, 3])
+
+newarr = arr.astype(bool)
 
 print(newarr)
 print(newarr.dtype)
