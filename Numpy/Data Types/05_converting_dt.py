0cb4d98cfd608691de289698a9f5b2c4cf3a93e9
akshay07-max <akshaysurase992@gmail.com>
2026-04-08 11:42:03 +0530

    changing datatypes of existing array astype()

---
+++ Numpy/Data Types/05_converting_dt.py
@@ -0,0 +1,10 @@
+# Converting Data Type on Existing Arrays
+
+import numpy as np
+
+# The best way to change the data type of an existing array, is to make a copy of the array with the astype() method.
+
+# The astype() function creates a copy of the array, and allows you to specify the data type as a parameter.
+
+# The data type can be specified using a string, like 'f' for float, 'i' for integer etc. or you can use the data type directly like float for float and int for integer.
+
