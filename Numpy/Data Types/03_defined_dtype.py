ef13e769a379a4a9d382ad61ec64708e5912d858
akshay07-max <akshaysurase992@gmail.com>
2026-04-08 10:01:19 +0530

    array default datatype assiging

--- Numpy/Data Types/03_defined_dtype.py
+++ Numpy/Data Types/03_defined_dtype.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+
+# Creating Arrays With a Defined Data Type
+# We use the array() function to create arrays, this function can take an optional argument: dtype that allows us to define the expected data type of the array elements:
 arr = np.array([12,3, 44, 55], dtype="S")
 
 print(arr.dtype)
+
+# For i, u, f, S and U we can define size as well.
