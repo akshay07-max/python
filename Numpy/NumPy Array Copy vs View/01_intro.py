f29f2ee725ce6e14768b2501b78aebc0c57ab045
akshay07-max <akshaysurase992@gmail.com>
2026-04-09 10:24:30 +0530

    copy

--- Numpy/NumPy Array Copy vs View/01_intro.py
+++ Numpy/NumPy Array Copy vs View/01_intro.py
@@ -3,3 +3,18 @@
 # The copy owns the data and any changes made to the copy will not affect original array, and any changes made to the original array will not affect the copy.
 
 # The view does not own the data and any changes made to the view will affect the original array, and any changes made to the original array will affect the view.
+
+
+# Copy:
+
+# Make a copy, change the original array, and display both arrays:
+import numpy as np
+
+arr = np.array([12, 45, 67, 89])
+
+x = arr.copy()
+
+arr[0] = 90
+
+print(arr)
+print(x)
