06e3976f16743382672db67e4498231709ce3274
akshay07-max <akshaysurase992@gmail.com>
2026-04-09 12:30:39 +0530

    experimenting view

---
+++ Numpy/NumPy Array Copy vs View/04_view.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr = np.array(["Apple", "Banana", "Orange", "Grapes", "Watermelon"])
+
+arr_view = arr.view()
+print(arr_view) # ['Apple' 'Banana' 'Orange' 'Grapes' 'Watermelon']
+
+# make changes in arr_view
+arr_view[0] = "Onion"
+print(arr_view) # ['Onion' 'Banana' 'Orange' 'Grapes' 'Watermelon']
+print(arr) # the original array get changed: ['Onion' 'Banana' 'Orange' 'Grapes' 'Watermelon']
+
+# conclusion: When you change anything with view the original array get affected unlike copy.
+
+# make changes in original array.
