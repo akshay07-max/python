7061bbfcabe5a82becfafc1676baddd8c7ef34c0
akshay07-max <akshaysurase992@gmail.com>
2026-04-12 12:32:49 +0530

    shape of an array

--- Numpy/Shape/01_intro.py
+++ Numpy/Shape/01_intro.py
@@ -6,3 +6,10 @@ arr = np.array([12, 43, 45, 66])
 shp = arr.shape
 
 print(shp) # (4,)
+
+# get the shape of an 2-D array
+
+tw_d = np.array([[122, 333, 444, 555], [555, 777, 888, 999]])
+
+print(tw_d.shape) # (2, 4)
+
