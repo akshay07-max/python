20988c6999b63a3ab537f17c0f5988b76d421640
akshay07-max <akshaysurase992@gmail.com>
2026-04-12 13:01:36 +0530

    array reshape

--- Numpy/Shape/03_reshape.py
+++ Numpy/Shape/03_reshape.py
@@ -5,6 +5,19 @@
 
 # By reshaping we can add or remove dimensions or change number of elements in each dimension.
 
+import numpy as np
+
+# Convert the following 1-D array with 12 elements into a 2-D array.
+
+# The outermost dimension will have 4 arrays, each with 3 elements:
+
+arr = np.array([1, 3, 4, 5, 6, 7, 9, 8, 90,80, 70, 60])
+
+new_arr = arr.reshape(4, 3) # what it says, create an 2-D array of the given array each having 3 elements in each and will have 4 arrays.
+
+print(new_arr)
+
+
 
 
 
