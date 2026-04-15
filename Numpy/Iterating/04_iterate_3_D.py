308850063376931bba3a74553ddf5486d713d7fa
akshay07-max <akshaysurase992@gmail.com>
2026-04-15 16:41:04 +0530

    iterating 3D array

--- Numpy/Iterating/04_iterate_3_D.py
+++ Numpy/Iterating/04_iterate_3_D.py
@@ -6,3 +6,12 @@ for x in arr:
     print (x)  # it will return two 2D array.
 
 
+for i in arr:
+    for j in i:
+        print(j) # it will return four 1-D arrays
+
+for x in arr:
+    for y in x:
+        for z in y:
+            print(z) # see the magic. All individual elements of an 3-D array are there.
+ 
