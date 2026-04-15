e79db02902ce56b27e0e8a6b831fa1704679667c
akshay07-max <akshaysurase992@gmail.com>
2026-04-15 09:44:31 +0530

    iterating introduction

---
+++ Numpy/Iterating/01_intro.py
@@ -0,0 +1,3 @@
+# iterating means going through elements one by one.
+# As we deal with multidimensional arrays in numpy, we can do this using basic for loop of python.
+# if we iterate on a 1-D array it will go through each element one by one. 
