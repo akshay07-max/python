61f3f30c04762a7effbb63579442ea03c409f9f3
akshay07-max <akshaysurase992@gmail.com>
2026-04-15 16:03:32 +0530

    reshape revision

--- Numpy/Shape/06_reshape.py
+++ Numpy/Shape/06_reshape.py
@@ -1,8 +1,12 @@
 import numpy as np
 
-arr = np.array([12, 32, 22, 22])
+# arr = np.array([12, 32, 22, 22])
 
-shape_it = arr.reshape(4, 1) 
+arr = np.array([12,221, 323, 12, 323, 333, 444, 332, 112, 231, 342, 222])
+
+shape_it = arr.reshape(3, 2, 2) 
+
+
 
 # [[12]
 #  [32]
@@ -10,3 +14,5 @@ shape_it = arr.reshape(4, 1)
 #  [22]]
 
 print(shape_it)
+
+
