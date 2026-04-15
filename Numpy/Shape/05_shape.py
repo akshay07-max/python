dd3731afde2504ae1ef707d1d96704e54ae90d1f
akshay07-max <akshaysurase992@gmail.com>
2026-04-15 09:57:38 +0530

    array shape practice

--- Numpy/Shape/05_shape.py
+++ Numpy/Shape/05_shape.py
@@ -2,8 +2,10 @@ import numpy as np
 
 
 # arr = np.array([12, 23, 43, 45, 21])
-arr = np.array([[12, 32, 434, 55, 12], [12, 23, 43, 45, 21]])
+# arr = np.array([[12, 32, 434, 55, 12], [12, 23, 43, 45, 21]])
+
+arr = np.array([[[122, 331, 222], [2223, 343, 122]], [[122, 321, 332], [321, 322, 122]], [[123, 223, 432], [3232, 4343, 5453]]])
 
 get_shape = arr.shape
 
-print(get_shape)
+print(get_shape) # (3, 2, 3) for 3-D array 3 is the elements of outer most dimension, 2 is 1 step inner, the elements of 2-D array which is inside, and so on.
