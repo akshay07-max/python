18641a55b1e1f80d1859fa2fc4afcbd1cecbc720
akshay07-max <akshaysurase992@gmail.com>
2026-04-17 10:25:50 +0530

    array joinig

---
+++ Numpy/Joining/01_intro.py
@@ -0,0 +1,3 @@
+# Joining means putting contents of two or more arrays in a single array.
+# In SQL we join tables based on a key, whereas in NumPy we join arrays by axes.
+# We pass a sequence of arrays that we want to join to the concatenate() function, along with the axis. If axis is not explicitly passed, it is taken as 0.
