acd630a675ed2203114397222b0be3666f37ed6a
akshay07-max <akshaysurase992@gmail.com>
2026-04-17 10:22:03 +0530

    iteration practice

--- Numpy/Iterating/07_iteration_pract.py
+++ Numpy/Iterating/07_iteration_pract.py
@@ -17,7 +17,13 @@ two_d = np.array([[12, 44, 53, 54], [23, 12, 43, 3]])
 # iterating 3-D array
 three_d = np.array([[[12, 43, 12, 34], [123, 432, 421, 432]], [[1234, 2342, 4212, 3422], [6534, 7534, 2322, 6432]]])
 
-for i in three_d:
-    for x in i:
-        for num in x:
-            print(num)
+# for i in three_d:
+#     for x in i:
+#         for num in x:
+#             print(num)
+
+
+# iterating using nditer
+
+for p in np.nditer(one_d):
+    print(p)
