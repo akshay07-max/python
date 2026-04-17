fdf0e71b6fdecb9ba1cf66ad45ccf7eb3f7dee67
akshay07-max <akshaysurase992@gmail.com>
2026-04-17 10:18:16 +0530

    iteration practice

--- Numpy/Iterating/07_iteration_pract.py
+++ Numpy/Iterating/07_iteration_pract.py
@@ -6,4 +6,9 @@ one_d = np.array(["Apple", "Papaya", "Banana", "Orange"])
 for f in one_d:
     print(f)
 
-    
+# iterating 2-D array
+two_d = np.array([[12, 44, 53, 54], [23, 12, 43, 3]])
+
+for x in two_d:
+    for n in x:
+        print(n)
