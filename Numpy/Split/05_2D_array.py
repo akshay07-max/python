434d9f99c840b12abef666905b4b1e35c1d8ed28
akshay07-max <akshaysurase992@gmail.com>
2026-04-20 10:40:03 +0530

    splitting 2D array

--- Numpy/Split/05_2D_array.py
+++ Numpy/Split/05_2D_array.py
@@ -8,5 +8,23 @@ arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
 
 newarr = np.array_split(arr, 3)
 
+# print(newarr)
+# print(newarr[0])
+
+# The example above returns three 2-D arrays.
+
+# Let's look at another example, this time each element in the 2-D arrays contains 3 elements.
+
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+
+newarr = np.array_split(arr, 3)
+
 print(newarr)
-print(newarr[0])
+
+# The example above returns three 2-D arrays.
+
+# In addition, you can specify which axis you want to do the split around.
+
+# The example below also returns three 2-D arrays, but they are split along the column (axis=1).
