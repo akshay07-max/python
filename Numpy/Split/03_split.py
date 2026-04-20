f03ace4fc7ca5152b8b68543e20d2e82da89c4a3
akshay07-max <akshaysurase992@gmail.com>
2026-04-20 10:34:39 +0530

    splitting

--- Numpy/Split/03_split.py
+++ Numpy/Split/03_split.py
@@ -7,3 +7,5 @@ arr = np.array([1, 2, 3, 4, 5, 6])
 newarr = np.array_split(arr, 4)
 
 print(newarr)
+
+# Note: We also have the method split() available but it will not adjust the elements when elements are less in source array for splitting like in example above, array_split() worked properly but split() would fail.
