f12f16370d14e8780e4fa9e12a872430928d30b5
akshay07-max <akshaysurase992@gmail.com>
2026-04-20 10:42:43 +0530

    hsplit

---
+++ Numpy/Split/06_hsplit.py
@@ -0,0 +1,11 @@
+# An alternate solution is using hsplit() opposite of hstack()
+
+# Use the hsplit() method to split the 2-D array into three 2-D arrays along columns.
+
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+
+newarr = np.hsplit(arr, 3)
+
+print(newarr)
