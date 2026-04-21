534c01488706c4a7d4d3aec357bb18731ac50ac3
Akshay Suarse <128041285+akshay07-max@users.noreply.github.com>
2026-04-21 23:50:04 +0530

    Create 03_21_04.py
    polymorphism

---
+++ 03_21_04.py
@@ -0,0 +1,11 @@
+#polymorphism
+class Cat:
+    def sound(self):
+        print("Meow")
+
+class Dog:
+    def sound(self):
+        print("Bark")
+
+for animal in [Cat(), Dog()]:
+    animal.sound()
