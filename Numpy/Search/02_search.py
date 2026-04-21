c8df9ab92c4392f2c4bd2d9e950e09b5df1cd7cb
akshay07-max <akshaysurase992@gmail.com>
2026-04-21 09:04:58 +0530

    searching

--- Numpy/Search/02_search.py
+++ Numpy/Search/02_search.py
@@ -5,3 +5,13 @@ arr = np.array([12, 32, 42, 53, 11, 12, 34, 12])
 duplicates = np.where(arr == 12)
 print (duplicates)
 
+for i in duplicates:
+    for j in i:
+        print(arr[j])
+
+get_evens = np.where(arr%2 == 0)
+print(get_evens)
+
+for i in get_evens:
+    for j in i:
+        print(f"Evens: {arr[j]}") 
