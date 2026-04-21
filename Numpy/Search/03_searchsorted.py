1e070e5503a4ce98b267416884a6bc2ad4295cd5
akshay07-max <akshaysurase992@gmail.com>
2026-04-21 09:18:41 +0530

    sorting by searchsort

--- Numpy/Search/03_searchsorted.py
+++ Numpy/Search/03_searchsorted.py
@@ -4,4 +4,15 @@ ar = np.array([12, 23, 44, 13])
 
 x = np.searchsorted(ar, 13)
 
-print(x)
+# print(x)
+
+sorted_arr = []
+
+for i in ar:
+    pos = np.searchsorted(sorted_arr, i)
+    sorted_arr.insert(pos, int(i))
+
+print(sorted_arr)
+
+
+
