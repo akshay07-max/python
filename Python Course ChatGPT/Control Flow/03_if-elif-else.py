5f03bee25bc2f8f2510a589c7c1a8dadd7165b2c
akshay07-max <akshaysurase992@gmail.com>
2026-04-22 12:31:36 +0530

    if-elif-else

---
+++ Python Course ChatGPT/Control Flow/03_if-elif-else.py
@@ -0,0 +1,18 @@
+# Used when multiple conditions
+
+marks = 36
+
+if marks >= 85:
+    print(f"congratulations... you got Grade 'A'")
+
+elif marks >= 75 and marks < 85:
+    print("Grade 'B'")
+
+elif marks >= 50 and marks < 75:
+    print("Grade 'B'")
+
+
+else:
+    print("Kya gunda banega re tuuuu....")
+
+
