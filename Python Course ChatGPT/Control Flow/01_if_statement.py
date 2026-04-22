041f82b29efebde3578b92fe790f3ddf1b00a32d
akshay07-max <akshaysurase992@gmail.com>
2026-04-22 12:00:24 +0530

    if statements

--- Python Course ChatGPT/Control Flow/01_if_statement.py
+++ Python Course ChatGPT/Control Flow/01_if_statement.py
@@ -1,8 +1,20 @@
 # Used when you want code to run only if condition is true.
 
+# Syntax:
+    # if condition:
+    #     code
+
+# Condition True? ---> Yes ---> Run Block
+#                ---> No ----> Skip
+
+age = 24
+
+if age >= 18:
+    print("You can drive now.")
+
 k = 221
 
-if(k%2 == 0):
+if k%2 == 0:
     print(f"The number {k} is even")
 
 print("Exited")
