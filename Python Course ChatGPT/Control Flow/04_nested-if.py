e6b1b6a26e49761ab21d73b2d21224144fe47c67
akshay07-max <akshaysurase992@gmail.com>
2026-04-22 12:48:46 +0530

    nested-if

---
+++ Python Course ChatGPT/Control Flow/04_nested-if.py
@@ -0,0 +1,13 @@
+age = 24
+salary = 40000
+
+if age >= 18:
+    if salary >= 35000:
+        print(f"Ohh at {age}, your are earning {salary} is good")
+    
+    if salary >= 25000:
+        print(f"Ohh salary good at this moment, I think your in good company")
+    else:
+        print("Salary is too less, Your in lala company boi")
+else:
+    print("Your a chid, enjoy you life, until some job or company ruin it.")
