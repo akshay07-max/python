32ae4211bd1e8718a5d21b28af3376eeeec2b5d7
akshay07-max <akshaysurase992@gmail.com>
2026-04-24 10:46:59 +0530

    comparision operators

---
+++ Python Course ChatGPT/Control Flow/05_conparision_operators.py
@@ -0,0 +1,14 @@
+a = 10
+b = 12
+
+if(a == b):
+    print(f"Both the numbers are equal")
+
+elif(a >= b):
+    print(f"Number {a} is greater than number {b}")
+
+elif(a <= b):
+    print(f"Number {a} is less than number {b}")
+
+else:
+    print("Something is wrong...")
