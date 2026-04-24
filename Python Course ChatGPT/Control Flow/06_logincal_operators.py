ecc77ce37ea0f1d485a7ae2f563bcf5afd95f872
akshay07-max <akshaysurase992@gmail.com>
2026-04-24 10:49:46 +0530

    comparision operators

--- Python Course ChatGPT/Control Flow/06_logincal_operators.py
+++ Python Course ChatGPT/Control Flow/06_logincal_operators.py
@@ -11,3 +11,11 @@ salary = 50000
 
 if age > 18 and salary > 30000:
     print("Eligible")
+# Both must be true.
+
+# or Example
+
+day = "Sunday"
+
+if day == "Sunday" or day == "Saturday":
+    print("Holiday")
