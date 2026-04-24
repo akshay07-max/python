b16badc399addaced917fa2a4d80a55aaf218117
akshay07-max <akshaysurase992@gmail.com>
2026-04-24 10:48:41 +0530

    logical operators

---
+++ Python Course ChatGPT/Control Flow/06_logincal_operators.py
@@ -0,0 +1,13 @@
+# Used to combine conditions.   
+
+# | Operator | Meaning      |
+# | -------- | ------------ |
+# | and      | Both true    |
+# | or       | Any one true |
+# | not      | Reverse      |
+
+age = 25
+salary = 50000
+
+if age > 18 and salary > 30000:
+    print("Eligible")
