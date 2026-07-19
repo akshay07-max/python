b15a880ab84900f4dcb4d90bb656798cb72a386d
akshay07-max <akshaysurase992@gmail.com>
2026-07-19 23:00:11 +0530

    Series:

---
+++ Pandas/03_series_pd.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# A pandas "Series" is like an column in table.
+# It hold one dimensional data of any type.
+
+my_data = [1234, 5423, 3421]
+
+get_series = pd.Series(my_data)
+print(get_series)
