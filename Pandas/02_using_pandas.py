8d106daa2c724e6e04667b3fca4704da21c2d606
akshay07-max <akshaysurase992@gmail.com>
2026-07-19 18:40:13 +0530

    version pandas

--- Pandas/02_using_pandas.py
+++ Pandas/02_using_pandas.py
@@ -1,10 +1,12 @@
-import pandas
+import pandas as pd
 
 my_data = {
     "Cars": ["Bugati", "BMW", "Mahindra", "Volvo"],
     "ratings": [9, 8, 9, 6]
 }
 
-analyse_data = pandas.DataFrame(my_data)
+analyse_data = pd.DataFrame(my_data)
 
 print(analyse_data)
+
+print(f"pandas version: {pd.__version__}")
