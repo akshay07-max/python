deeb4550597bddf8a6bf63ad65fa83b5876fca63
akshay07-max <akshaysurase992@gmail.com>
2026-07-19 18:31:33 +0530

    Started on pandas

---
+++ Pandas/01_intro.py
@@ -0,0 +1,17 @@
+# Pandas is used to analyse data
+
+# python library used to work with data.
+# I has functions for analyse, cleaning, exploring and manipulating the data.
+
+
+# What Can Pandas Do?
+# Pandas gives you answers about the data. Like:
+
+# Is there a correlation between two or more columns?
+# What is average value?
+# Max value?
+# Min value?
+
+# Where is the Pandas Codebase?
+# The source code for Pandas is located at this github repository https://github.com/pandas-dev/pandas
+
