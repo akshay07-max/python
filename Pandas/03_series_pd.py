e033ff208d0817958d57c069bafea6870aae9dc1
akshay07-max <akshaysurase992@gmail.com>
2026-07-19 23:07:00 +0530

    git stash

--- Pandas/03_series_pd.py
+++ Pandas/03_series_pd.py
@@ -7,3 +7,9 @@ my_data = [1234, 5423, 3421]
 
 get_series = pd.Series(my_data)
 print(get_series)
+
+
+# Labels:
+# The label are basically a index of the data (If nothing is specified). The index starts form 0, 1 etc.
+# The label can be used to access a specified element.
+
